ecb172835de422394e547d13fcbde60947f4928d
miodrag.lazic0204@gmail.com <miodrag.lazic0204@gmail.com>
2021-05-18 12:43:44 +0200

    Final version - 3

--- dangerjs/version.js
+++ dangerjs/version.js
@@ -5,24 +5,24 @@ const errors = {
 }
 
 // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-const REGEX_VERSION_QUERY = /"version": "(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"/;
+const REGEX_VERSION_QUERY = /"version": "(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"/
 
 function checkForVersionUpdate ({ versionValidation, packageJsonDiff, packageLockJsonDiff }) {
   if (!versionValidation) return
 
-  const isPackageJsonChanged = packageJsonDiff && packageJsonDiff.added;
-  const isPackageLockJsonChanged = packageLockJsonDiff && packageLockJsonDiff.added;
+  const isPackageJsonChanged = packageJsonDiff && packageJsonDiff.added
+  const isPackageLockJsonChanged = packageLockJsonDiff && packageLockJsonDiff.added
 
-  const isNewVersionSet = isPackageJsonChanged && packageJsonDiff.added.match(REGEX_VERSION_QUERY);
+  const isNewVersionSet = isPackageJsonChanged && packageJsonDiff.added.match(REGEX_VERSION_QUERY)
 
-  if(!isNewVersionSet) return errors.versionNotBumped;
+  if(!isNewVersionSet) return errors.versionNotBumped
  
-  if(!isPackageLockJsonChanged) return errors.versionNotUpdatedInLockFile;
+  if(!isPackageLockJsonChanged) return errors.versionNotUpdatedInLockFile
 
-  const packageJsonNewVersion = packageJsonDiff.added.match(REGEX_VERSION_QUERY)[0];
-  const packageLockJsonNewVersion = packageLockJsonDiff.added.match(REGEX_VERSION_QUERY)[0];
+  const packageJsonNewVersion = packageJsonDiff.added.match(REGEX_VERSION_QUERY)[0]
+  const packageLockJsonNewVersion = packageLockJsonDiff.added.match(REGEX_VERSION_QUERY)[0]
 
-  if(packageJsonNewVersion !== packageLockJsonNewVersion) return errors.versionMissmatch;
+  if(packageJsonNewVersion !== packageLockJsonNewVersion) return errors.versionMissmatch
 }
 
 module.exports = {
